37c3a91bce6b6518ac5a5b46cd4c38f8b3e8045f
supermanlas <esteban_5.4@hotmail.com>
2020-01-08 13:03:24 -0300

    Subir aplicacion contador

---
+++ contador/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:contador/src/app.dart';
+
+//Nosotros escribimos
+//import 'src/app.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+
+  runApp(new MyApp());
+
+
+}
+
+
+
